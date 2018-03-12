0f4827fa92a8b13e5c72858847cf9b62a8f78f8e
guobing1993 <1131788816@qq.com>
2018-03-12 13:12:31 +0800

    props

---
+++ 06-02/src/js/components/indexBody.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+
+
+
+
+export default class Body extends React.Component {
+
+    constructor() {
+        super();
+        this.state = { //初始化赋值
+            username: "guobing"
+        }
+    };
+
+    render() {
+
+        setTimeout(() => {
+            this.setState({
+                username: "guoyu"
+            })
+        }, 4000);
+        return (
+            <section>
+                <h1>这里是主体</h1>
+                <p>{this.state.username}</p>
+                <p>{this.props.userId}</p>
+            </section>
+        )
+    }
+
+
+}
---
+++ 06-02/src/js/index.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Header from "./components/header.jsx";
+import Footer from "./components/footer.jsx";
+import Body from "./components/indexBody.jsx"
+
+class Index extends React.Component {
+    componentWillMount() {
+        console.log("===================这是index页面开始加载===================")
+    };
+
+    componentDidMount() {
+        console.log("===================这是index页面加载完了======================")
+    };
+    render() {
+        var h = <Header />;
+        return (
+            <div>
+                {h}    {/* 参数形式调用 */}
+                <Body userId="123456" />
+                <Footer></Footer>
+
+            </div>
+        )
+    }
+}
+ReactDOM.render(
+    <Index />,
+    document.getElementById("example")
+)
