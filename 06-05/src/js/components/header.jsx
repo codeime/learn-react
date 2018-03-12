69cb2335eee9892e1e0b437c77ac2ee57de7e912
guobing1993 <1131788816@qq.com>
2018-03-12 22:06:46 +0800

    refs

---
+++ 06-05/src/js/components/header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+
+
+export default class Footer extends React.Component {
+    render() {
+        return (
+            <footer>
+                <h1>这里是页面的的头部</h1>
+            </footer>
+        )
+    }
+
+
+}
+
