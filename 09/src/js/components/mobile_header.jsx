e2f00eb7855665a01aca04a0b0f0082932a3f226
guobing1993 <qq.1131788816@live.com>
2018-03-25 23:11:45 +0800

    login

--- 09/src/js/components/mobile_header.jsx
+++ 09/src/js/components/mobile_header.jsx
@@ -1,25 +1,92 @@
 import React from 'react';
 import { Row, Col } from "antd";
 
-import { Menu, Icon } from 'antd';
+import { Menu, Icon, Tabs, message, Form, Input, Button, CheckBox, Modal } from 'antd';
+const FormItem = Form.Item;
 const SubMenu = Menu.SubMenu;
 const MenuItemGroup = Menu.ItemGroup;
+const TabPane = Tabs.TabPane;
 
-export default class MobileHeader extends React.Component {
+
+class MobileHeader extends React.Component {
     constructor() {
         super();
         this.state = {
-            current: "app"
+            current: "app",
+            action: "login",
+            hasLogined: false,
+            userNickName: '',
+            userId: ""
+
         }
     };
+    handleClick(e) {
+        if (e.key = "register") {
+            this.setState({
+                current: "register"
+            });
+            this.setModalVisible(true);
+        } else {
+            this.setState({
+                current: e.key
+            });
 
+        }
+    };
+    setModalVisible(value) {
+        this.setState({
+            modalVisible: value
+        })
+    };
+    handleSubmit(e) {
+        e.preventDefault();
+        var myFetchOptions = {
+            method: "GET"
+        };
+        var formData = this.props.form.getFieldProps;
+        fetch()
+            .then()
+            .then()
+        this.setModalVisible(false);
+    };
+    login() {
+        this.setModalVisible(true)
+    }
     render() {
+        let { getFieldProps } = this.props.form;
+
+        const userShow = this.state.haslogined ?
+            <Link>
+                <Icon type="inbox"></Icon>
+            </Link>
+            :
+            <Icon type="setting" onClick={this.login.bind(this)}></Icon>
+            ;
         return (
             <header >
                 <img src="./src/img/logo.png" alt="logo" />
                 <span>ReactNews</span>
+                {userShow}
+                <Modal title="用户中心" wrapClassName="vertial-center-modal" visible={this.state.modalVisible} onCancel={() => this.setModalVisible(false)
+                } onOk={() => this.setModalVisible(false)} okText='关闭'>
+                    <Tabs type="card">
+                        <Form horizontal onSubmit={this.handleSubmit.bind(this)}>
+                            <FormItem label="账户">
+                                <Input placeholder="请输入账号" {...getFieldProps("r_username")}></Input>
+                            </FormItem>
+                            <FormItem label="密码">
+                                <Input type="password" placeholder="请输入密码" {...getFieldProps("r_password")}></Input>
+                            </FormItem>
+                            <FormItem label="确认密码">
+                                <Input type="password" placeholder="请再次输入密码" {...getFieldProps("r_confirmpassword")}></Input>
+                            </FormItem>
+                            <Button type="primary" htmlType="submit">注册</Button>
+                        </Form>
+                    </Tabs>
+                </Modal>
             </header >
 
         )
     }
-} 
+}
+export default MobileHeader = Form.create({})(MobileHeader);
--- 09/src/js/components/pc_header.jsx
+++ 09/src/js/components/pc_header.jsx
@@ -1,19 +1,74 @@
 import React from 'react';
 import { Row, Col } from "antd";
 
-import { Menu, Icon } from 'antd';
+import { Menu, Icon, Tabs, message, Form, Input, Button, CheckBox, Modal } from 'antd';
+const FormItem = Form.Item;
 const SubMenu = Menu.SubMenu;
 const MenuItemGroup = Menu.ItemGroup;
-
-export default class PCHeader extends React.Component {
+const TabPane = Tabs.TabPane;
+class PCHeader extends React.Component {
     constructor() {
         super();
         this.state = {
-            current: "app"
+            current: "app",
+            action: "login",
+            hasLogined: false,
+            userNickName: '',
+            userId: ""
+
         }
     };
+    handleClick(e) {
+        if (e.key = "register") {
+            this.setState({
+                current: "register"
+            });
+            this.setModalVisible(true);
+        } else {
+            this.setState({
+                current: e.key
+            });
 
+        }
+    };
+    setModalVisible(value) {
+        this.setState({
+            modalVisible: value
+        })
+    };
+    handleSubmit(e) {
+        e.preventDefault();
+        var myFetchOptions = {
+            method: "GET"
+        };
+        var formData = this.props.form.getFieldProps;
+        fetch()
+            .then()
+            .then()
+        this.setModalVisible(false);
+    };
     render() {
+        let { getFieldProps } = this.props.form;
+        let userShow = this.state.hasLogined ?
+            <Menu.Item key="logout" class="register">
+                <Button type="primary" htmlType="button">{this.state.userNickName}</Button>
+                &nbsp;
+                &nbsp;
+                <Link target="_blank">
+                    <Button type="dashed" htmlType="button">个人中心</Button>
+                </Link>
+                &nbsp;
+                &nbsp;
+                <Button type="dashed" htmlType="button" onclick="">退出</Button>
+            </Menu.Item>
+            :
+            <Menu.Item>
+                <Icon type="appstore" key="register" />注册/登陆
+            </Menu.Item>;
+
+
+
+
         return (
             <header>
                 <Row>
@@ -25,7 +80,7 @@ export default class PCHeader extends React.Component {
                         </a>
                     </Col>
                     <Col span={16}>
-                        <Menu mode="horizontal" selectedKeys={[this.state.current]} onClick={this.handleClick}>
+                        <Menu mode="horizontal" onClick={this.handleClick.bind(this)} selectedKeys={[this.state.current]} >
                             <Menu.Item key="app">
                                 <Icon type="appstore" />头条
                             </Menu.Item>
@@ -47,12 +102,32 @@ export default class PCHeader extends React.Component {
                             <Menu.Item key="shishang">
                                 <Icon type="appstore" />时尚
                             </Menu.Item>
+                            {userShow}
                         </Menu>
+
+                        <Modal title="用户中心" wrapClassName="vertial-center-modal" visible={this.state.modalVisible} onCancel={() => this.setModalVisible(false)
+                        } onOk={() => this.setModalVisible(false)} okText='关闭'>
+                            <Tabs type="card">
+                                <Form horizontal onSubmit={this.handleSubmit.bind(this)}>
+                                    <FormItem label="账户">
+                                        <Input placeholder="请输入账号" {...getFieldProps("r_username")}></Input>
+                                    </FormItem>
+                                    <FormItem label="密码">
+                                        <Input type="password" placeholder="请输入密码" {...getFieldProps("r_password")}></Input>
+                                    </FormItem>
+                                    <FormItem label="确认密码">
+                                        <Input type="password" placeholder="请再次输入密码" {...getFieldProps("r_confirmpassword")}></Input>
+                                    </FormItem>
+                                    <Button type="primary" htmlType="submit">注册</Button>
+                                </Form>
+                            </Tabs>
+                        </Modal>
                     </Col>
                     <Col span={2}></Col>
                 </Row>
-            </header>
+            </header >
 
         )
     }
 }
+export default PCHeader = Form.create({})(PCHeader);
