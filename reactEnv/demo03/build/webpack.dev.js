9ee3434a26834f4ef2ef23e5849ba564acb77b74
guobing1993 <qq.1131788816@live.com>
2018-08-21 09:24:45 +0800

    modify webpack config

--- reactEnv/demo03/build/webpack.dev.js
+++ reactEnv/demo03/build/webpack.dev.js
@@ -5,6 +5,7 @@ const path = require('path');
 
 
 module.exports = merge(base, {
+    mode:'development',
     devtool: 'inline-source-map',
     devServer: {
         contentBase: path.resolve(__dirname, "../dist"),
