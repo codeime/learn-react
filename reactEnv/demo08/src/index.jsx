ff2a7df8f8559d85d53b3dc80a1e19570959a6c3
guobing1993 <qq.1131788816@live.com>
2018-08-28 09:07:28 +0800

    remove some dependence

--- reactEnv/demo08/src/index.jsx
+++ reactEnv/demo08/src/index.jsx
@@ -4,26 +4,13 @@ console.log(process.env.NODE_ENV);
 
 import React from 'react';
 import { render } from 'react-dom';
+import { hashHistory } from "react-router-dom";
 
 import '@/static/style/common.less';
 
-import Hello from '@/page/Hello/index';
-import ToDo from '@/page/ToDo/index';
-
-class Index extends React.Component {
-    render() {
-        return (
-            <div>
-                <ToDo></ToDo>
-                {/*  <Hello ></Hello> */}
-                {/* jsx注释 */}
-            </div >
-        );
-    }
-
-}
+import RouterMap from './router/routerMap'
 
 render(
-    <Index />,
-    document.getElementById('root'),
+    <RouterMap history={hashHistory} />,
+    document.getElementById('root')
 );
