f098a92c26c66b603c7b0fb0d896df86ba017cc2
guobing1993 <qq.1131788816@live.com>
2018-08-29 19:46:55 +0800

    react-router params

--- reactEnv/demo08/src/page/Detail/index.jsx
+++ reactEnv/demo08/src/page/Detail/index.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 
 class Detail extends React.Component {
+    constructor(props, context) {
+        super(props, context)
+    }
     render() {
+        let params = this.props.match.params
         return (
             <div>
-                详情
+                详情{params.id}
             </div>
         );
     }
