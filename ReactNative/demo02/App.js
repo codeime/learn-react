3f1c6ae62c5419105ec3b4c8dc0ba326815b16a6
guobing <qq.1131788816@live.com>
2018-12-22 18:34:33 +0800

    new setting

--- ReactNative/demo02/App.js
+++ ReactNative/demo02/App.js
@@ -6,35 +6,34 @@
  * @flow
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import React, { Component } from 'react';
+import { Platform, StyleSheet, Text, View } from 'react-native';
 
 const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+    ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+    android: 'Double tap R on your keyboard to reload,\n' +
+        'Shake or press menu button for dev menu',
 });
 
 type Props = {};
 export default class HelloWorld extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.text}>Hello world</Text>
-      </View>
-    );
-  }
+    render() {
+        return (
+            <View style={styles.container} >
+                <Text style={styles.text} > Hello world test </Text>
+            </View >
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    backgroundColor: 'red',
-  },
-  text: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-    backgroundColor:'green'
-  }
+    container: {
+        backgroundColor: 'red',
+    },
+    text: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+        backgroundColor: 'green'
+    }
 });
